7f5b252b52521d1e9de5f654ef15f65f3f36c6f6
AirOneX01 <aironex@icloud.com>
2024-06-28 13:23:44 +0300

    ver fin commit1

---
+++ My Score/Models/StatsModel.swift
@@ -0,0 +1,8 @@
+//
+//  StatsModel.swift
+//  My Score
+//
+//  Created by Air One on 6/27/24.
+//
+
+import Foundation
---
+++ My Score/Views/StatCell.swift
@@ -0,0 +1,23 @@
+//
+//  StatCell.swift
+//  My Score
+//
+//  Created by Air One on 6/27/24.
+//
+
+import UIKit
+
+class StatCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
